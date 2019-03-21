5301527ad83d3b7a89dfdf7683dd1ca6bb08aa4c
WillieT1986 <warmad2@hotmail.com>
2019-03-21 13:24:32 -0400

    Initial Commit

---
+++ src/main/java/com/wrthompsonjr/greenlawn/GreenlawnApplication.java
@@ -0,0 +1,13 @@
+package com.wrthompsonjr.greenlawn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenlawnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreenlawnApplication.class, args);
+	}
+
+}
