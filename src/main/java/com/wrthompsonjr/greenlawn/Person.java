087dba621affe79986d44af6f6bc6d7a9bd49041
WillieT1986 <warmad2@hotmail.com>
2019-03-22 00:02:05 -0400

    Added String name. Changed null to name. Will allow any name to be retrieved.

--- src/main/java/com/wrthompsonjr/greenlawn/Person.java
+++ src/main/java/com/wrthompsonjr/greenlawn/Person.java
@@ -2,13 +2,14 @@ package com.wrthompsonjr.greenlawn;
 
 public class Person {
 
-	public Person(String string) {
-		// TODO Auto-generated constructor stub
+	private String name;
+
+	public Person(String name) {
+		this.name = name;
 	}
 
 	public String getName() {
-		// TODO Auto-generated method stub
-		return null;
+		return name;
 	}
 
 }
