c8111606fab67e9150302637801e6ed731a7e38f
WillieT1986 <warmad2@hotmail.com>
2019-03-23 01:08:22 -0400

    Made it so when cemeterySection is called upon then the correct cemeterySection will return. For example, when calling on 101 then the return will be 101 and not 0.

--- src/main/java/com/wrthompsonjr/greenlawn/CemeterySection.java
+++ src/main/java/com/wrthompsonjr/greenlawn/CemeterySection.java
@@ -2,13 +2,14 @@ package com.wrthompsonjr.greenlawn;
 
 public class CemeterySection {
 
-	public CemeterySection(int i) {
-		// TODO Auto-generated constructor stub
+	private int cemeterySection;
+
+	public CemeterySection(int cemeterySection) {
+		this.cemeterySection = cemeterySection;
 	}
 
 	public int getCemeterySection() {
-		// TODO Auto-generated method stub
-		return 0;
+		return cemeterySection;
 	}
 
 }
