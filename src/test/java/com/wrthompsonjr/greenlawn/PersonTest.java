79971f8b7035e02c6261ad882edeea9743547c46
WillieT1986 <warmad2@hotmail.com>
2019-03-23 01:16:02 -0400

    Through testing, getCemeterySection was added. Ran test. Test Failed. Expected 101 but was 0.

--- src/test/java/com/wrthompsonjr/greenlawn/PersonTest.java
+++ src/test/java/com/wrthompsonjr/greenlawn/PersonTest.java
@@ -9,42 +9,49 @@ public class PersonTest {
 
 	@Test
 	public void shouldInstantiateAPerson() {
-		Person underTest = new Person("", "", "", "", "");
+		Person underTest = new Person("", "", "", "", "", 0);
 		assertNotNull(underTest);
 	}
 
 	@Test
 	public void shouldConstructAndReturnPersonName() {
-		Person underTest = new Person("name", "", "", "", "");
+		Person underTest = new Person("name", "", "", "", "", 0);
 		String check = underTest.getName();
 		assertEquals("name", check);
 	}
 
 	@Test
 	public void shouldReturnPersonDateOfBirth() {
-		Person underTest = new Person("", "DOB", "", "", "");
+		Person underTest = new Person("", "DOB", "", "", "", 0);
 		String check = underTest.getDateOfBirth();
 		assertEquals("DOB", check);
 	}
 
 	@Test
 	public void shouldReturnPersonDateOfDeath() {
-		Person underTest = new Person("", "", "DOD", "", "");
+		Person underTest = new Person("", "", "DOD", "", "", 0);
 		String check = underTest.getDateOfDeath();
 		assertEquals("DOD", check);
 	}
 
 	@Test
 	public void shouldReturnPersonStatus() {
-		Person underTest = new Person("", "", "", "status", "");
+		Person underTest = new Person("", "", "", "status", "", 0);
 		String check = underTest.getStatus();
 		assertEquals("status", check);
 	}
 
 	@Test
 	public void shouldReturnPersonDescription() {
-		Person underTest = new Person("", "", "", "", "Description");
+		Person underTest = new Person("", "", "", "", "Description", 0);
 		String check = underTest.getDrescription();
 		assertEquals("Description", check);
+	}
+
+	@Test
+	public void shouldReturnCemeterySection() {
+		Person underTest = new Person("", "", "", "", "", 101);
+		int check = underTest.getCemeterySection();
+		assertEquals(101, check);
 	}
 }
