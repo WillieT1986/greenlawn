de14f0e1b508efd0215ada96b15a161337b900f1
WillieT1986 <warmad2@hotmail.com>
2019-03-23 22:42:25 -0400

    Ran 1st Test. Test crashed. Failed to load ApplicationContext. Error creating bean with name 'personRepository'.

--- src/test/java/com/wrthompsonjr/greenlawn/JpaMappingTest.java
+++ src/test/java/com/wrthompsonjr/greenlawn/JpaMappingTest.java
@@ -1,11 +1,36 @@
 package com.wrthompsonjr.greenlawn;
 
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+import javax.annotation.Resource;
+
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @DataJpaTest
 public class JpaMappingTest {
 
+	@Resource
+	private TestEntityManager entityManager;
+
+	@Resource
+	private PersonRepository personRepo;
+
+	@Test
+	public void shouldSaveAndLoadPerson() {
+		Person person = new Person("personName", "DateOfBirth", "DateOfDeath", "Status", "Description");
+		person = personRepo.save(person);
+		long personId = person.getId();
+
+		entityManager.flush();
+		entityManager.clear();
+
+		person = personRepo.findOne(personId);
+		assertThat(person.getName(), is("personName"));
+	}
 }
