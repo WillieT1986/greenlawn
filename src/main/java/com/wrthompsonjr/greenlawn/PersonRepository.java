7569a4221d35e5e8e02685cf6c7c05613e575445
WillieT1986 <warmad2@hotmail.com>
2019-03-23 22:47:25 -0400

    Changed to CrudRepository. Re-ran 1st Test. Test Failed. Same Error(s).

--- src/main/java/com/wrthompsonjr/greenlawn/PersonRepository.java
+++ src/main/java/com/wrthompsonjr/greenlawn/PersonRepository.java
@@ -1,10 +1,10 @@
 package com.wrthompsonjr.greenlawn;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PersonRepository extends JpaRepository<Person, Long> {
+public interface PersonRepository extends CrudRepository<Person, Long> {
 
 	Person findOne(long personId);
 
