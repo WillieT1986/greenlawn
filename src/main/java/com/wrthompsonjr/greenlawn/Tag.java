8565345001de7139384121781e12aa00d4afc261
WillieT1986 <warmad2@hotmail.com>
2019-03-23 21:23:15 -0400

    Created Tag, Tag constructor and getter through testing.

---
+++ src/main/java/com/wrthompsonjr/greenlawn/Tag.java
@@ -0,0 +1,14 @@
+package com.wrthompsonjr.greenlawn;
+
+public class Tag {
+
+	public Tag(String string) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getTag() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
