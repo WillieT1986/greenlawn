bb35c288a10af9acf4f2148ab88c658c17def499
WillieT1986 <warmad2@hotmail.com>
2019-03-26 14:34:03 -0400

    Created through testing.

---
+++ src/main/java/com/wrthompsonjr/greenlawn/TagRepository.java
@@ -0,0 +1,10 @@
+package com.wrthompsonjr.greenlawn;
+
+import javax.annotation.Resource;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+@Resource
+public interface TagRepository extends JpaRepository<Tag, Long> {
+
+}
