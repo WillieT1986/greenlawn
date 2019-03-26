a25683a86fda0afc8c844380f0f429ea727538f0
WillieT1986 <warmad2@hotmail.com>
2019-03-25 23:39:25 -0400

    Changed CrudRepository to JpaRepository.

--- src/main/java/com/wrthompsonjr/greenlawn/PersonRepository.java
+++ src/main/java/com/wrthompsonjr/greenlawn/PersonRepository.java
@@ -1,11 +1,9 @@
 package com.wrthompsonjr.greenlawn;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PersonRepository extends CrudRepository<Person, Long> {
-
-	Person findOne(long personId);
+public interface PersonRepository extends JpaRepository<Person, Long> {
 
 }
