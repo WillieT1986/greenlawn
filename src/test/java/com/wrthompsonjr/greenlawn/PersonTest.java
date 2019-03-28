a91cd43522d8946976291ea2369b3e8b92affbf6
WillieT1986 <warmad2@hotmail.com>
2019-03-28 14:14:19 -0400

    Passed Test to add String religion to Person constructor. Updated all tests. Ran All Tests for PersonTest. Test Passed.

--- src/test/java/com/wrthompsonjr/greenlawn/PersonTest.java
+++ src/test/java/com/wrthompsonjr/greenlawn/PersonTest.java
@@ -9,42 +9,49 @@ public class PersonTest {
 
 	@Test
 	public void shouldInstantiateAPerson() {
-		Person underTest = new Person("", "", "", "", "", null);
+		Person underTest = new Person("", "", "", "", "", null, "");
 		assertNotNull(underTest);
 	}
 
 	@Test
 	public void shouldConstructAndReturnPersonName() {
-		Person underTest = new Person("name", "", "", "", "", null);
+		Person underTest = new Person("name", "", "", "", "", null, "");
 		String check = underTest.getName();
 		assertEquals("name", check);
 	}
 
 	@Test
 	public void shouldReturnPersonDateOfBirth() {
-		Person underTest = new Person("", "DOB", "", "", "", null);
+		Person underTest = new Person("", "DOB", "", "", "", null, "");
 		String check = underTest.getDateOfBirth();
 		assertEquals("DOB", check);
 	}
 
 	@Test
 	public void shouldReturnPersonDateOfDeath() {
-		Person underTest = new Person("", "", "DOD", "", "", null);
+		Person underTest = new Person("", "", "DOD", "", "", null, "");
 		String check = underTest.getDateOfDeath();
 		assertEquals("DOD", check);
 	}
 
 	@Test
 	public void shouldReturnPersonStatus() {
-		Person underTest = new Person("", "", "", "status", "", null);
+		Person underTest = new Person("", "", "", "status", "", null, "");
 		String check = underTest.getStatus();
 		assertEquals("status", check);
 	}
 
 	@Test
 	public void shouldReturnPersonDescription() {
-		Person underTest = new Person("", "", "", "", "Description", null);
+		Person underTest = new Person("", "", "", "", "Description", null, "");
 		String check = underTest.getDrescription();
 		assertEquals("Description", check);
+	}
+
+	@Test
+	public void shouldAddReligionToPerson() {
+		Person underTest = new Person("", "", "", "", "", null, "Religion");
+		String check = underTest.getReligion();
+		assertEquals("Religion", check);
 	}
 }
