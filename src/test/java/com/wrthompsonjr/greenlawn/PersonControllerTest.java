58b2da1dd114dd883cbb60c46ec3b6d2af24feb0
WillieT1986 <warmad2@hotmail.com>
2019-03-27 20:50:30 -0400

    Ran Mock Test. Test Failed.

---
+++ src/test/java/com/wrthompsonjr/greenlawn/PersonControllerTest.java
@@ -0,0 +1,44 @@
+package com.wrthompsonjr.greenlawn;
+
+import static org.hamcrest.Matchers.any;
+import static org.hamcrest.Matchers.contains;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.when;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+public class PersonControllerTest {
+
+	@InjectMocks
+	private PersonRestController underTest;
+
+	@Mock
+	private Person person;
+
+	@Mock
+	private PersonRepository personRepo;
+
+	@Mock
+	private Collection<Person> persons;
+
+	@Before
+	public void setUp() {
+		MockitoAnnotations.initMocks(this);
+	}
+
+	@Test
+	public void shouldGetPersonFromDatabase() {
+		when(personRepo.findAll()).thenReturn((List<Person>) Collections.singleton(person));
+		Iterable<Person> result = underTest.findPersons();
+		assertThat(result, contains(any(Person.class)));
+	}
+
+}
