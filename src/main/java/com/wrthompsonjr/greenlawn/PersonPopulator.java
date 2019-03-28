ab10ceae2b61b2888163e36317061fc4e99d49a8
WillieT1986 <warmad2@hotmail.com>
2019-03-28 00:42:32 -0400

    Created PersonPopulator. Added Person and CemeterySection Repo's. Added for a way to save an individual Cemetery Section manually. Ran into problem. A Cemetery also has letters. Need to go back to CemeterySection class and refactor from an int to a String.

---
+++ src/main/java/com/wrthompsonjr/greenlawn/PersonPopulator.java
@@ -0,0 +1,29 @@
+package com.wrthompsonjr.greenlawn;
+
+import javax.annotation.Resource;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PersonPopulator implements CommandLineRunner {
+
+	@Resource
+	PersonRepository personRepo;
+
+	@Resource
+	CemeterySectionRepository sectionRepo;
+
+	private CemeterySection createCemeterySection(int cemeterySectionArea) {
+		CemeterySection z = new CemeterySection(cemeterySectionArea);
+		return sectionRepo.save(z);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		CemeterySection sectionA = createCemeterySection(101);
+
+	}
+
+}
