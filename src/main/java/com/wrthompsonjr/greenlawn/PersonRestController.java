449d0f3816039eef330e95d2c918183f2fd24074
WillieT1986 <warmad2@hotmail.com>
2019-03-27 22:18:44 -0400

    Created exception message through testing.

--- src/main/java/com/wrthompsonjr/greenlawn/PersonRestController.java
+++ src/main/java/com/wrthompsonjr/greenlawn/PersonRestController.java
@@ -2,8 +2,10 @@ package com.wrthompsonjr.greenlawn;
 
 import javax.annotation.Resource;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -26,6 +28,20 @@ public class PersonRestController {
 			throw new Exception("Person Not Found");
 		}
 		return personRepo.getOne(id);
+	}
+
+	@SuppressWarnings("serial")
+	@ResponseStatus(HttpStatus.NOT_FOUND)
+	public class CannotFoundException extends RuntimeException {
+		private String message;
+
+		public String getMessage() {
+			return message;
+		}
+
+		CannotFoundException(String errorMessage) {
+			this.message = errorMessage;
+		}
 	}
 
 }
