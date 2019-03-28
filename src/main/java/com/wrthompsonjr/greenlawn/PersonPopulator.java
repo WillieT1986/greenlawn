d72b3ad25684c722900a61cbb7dc1d8574b3c12e
WillieT1986 <warmad2@hotmail.com>
2019-03-28 13:23:36 -0400

    Added Tags to create Tags. Created first sample Person.

--- src/main/java/com/wrthompsonjr/greenlawn/PersonPopulator.java
+++ src/main/java/com/wrthompsonjr/greenlawn/PersonPopulator.java
@@ -14,9 +14,17 @@ public class PersonPopulator implements CommandLineRunner {
 	@Resource
 	CemeterySectionRepository sectionRepo;
 
+	@Resource
+	TagRepository tagRepo;
+
 	private CemeterySection createCemeterySection(String cemeterySectionArea) {
 		CemeterySection z = new CemeterySection(cemeterySectionArea);
 		return sectionRepo.save(z);
+	}
+
+	private Tag createTag(String tagName) {
+		Tag t = new Tag(tagName);
+		return tagRepo.save(t);
 	}
 
 	@Override
@@ -24,6 +32,15 @@ public class PersonPopulator implements CommandLineRunner {
 
 		CemeterySection sectionA = createCemeterySection("123B"); // Civilians
 		CemeterySection sectionB = createCemeterySection("28"); // Veterans
+
+		Tag tagOne = createTag("WWI");
+		Tag tagTwo = createTag("WWII");
+		Tag tagThree = createTag("Spanish American War");
+		Tag tagFour = createTag("Civilian");
+
+		// 10 sample Civilians
+		personRepo.save(new Person("John A Doe", "01/15/1965", "12/15/2015", "", "No Summary At This Time...", sectionA,
+				tagFour));
 
 	}
 
