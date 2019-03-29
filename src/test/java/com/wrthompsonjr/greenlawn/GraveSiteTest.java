d0c8b9f381222d2455b366d478a838d6e605d6f1
WillieT1986 <warmad2@hotmail.com>
2019-03-28 23:18:26 -0400

    Refactored and Polished (Minor Edits)

--- src/test/java/com/wrthompsonjr/greenlawn/GraveSiteTest.java
+++ src/test/java/com/wrthompsonjr/greenlawn/GraveSiteTest.java
@@ -6,7 +6,7 @@ import static org.junit.Assert.assertNotNull;
 import org.junit.Before;
 import org.junit.Test;
 
-public class PersonTest {
+public class GraveSiteTest {
 
 	private static final String TOMBSTONE_IMAGE_URL = "Tombstone Image";
 	private static final String NAME_OF_PERSON = "name";
@@ -17,85 +17,85 @@ public class PersonTest {
 	private static final String DATE_OF_BIRTH = "DOB";
 	private static final String DATE_OF_DEATH = "DOD";
 	private static final String STATUS = "Status";
-	private static final String DESCRIPTION = "Description";
+	private static final String OBITUARY = "Description";
 	private static final CemeterySection CEMETERY_SECTION = null;
 	private static final String RELIGION = "Religion";
 
-	Person underTest;
+	GraveSite underTest;
 
 	@Before
 	public void setUp() {
-		underTest = new Person(TOMBSTONE_IMAGE_URL, NAME_OF_PERSON, US_STATE, MILITARY_RANK, MILITARY_BRANCH,
-				MILITARY_UNIT, DATE_OF_BIRTH, DATE_OF_DEATH, STATUS, DESCRIPTION, CEMETERY_SECTION, RELIGION);
+		underTest = new GraveSite(TOMBSTONE_IMAGE_URL, NAME_OF_PERSON, US_STATE, MILITARY_RANK, MILITARY_BRANCH,
+				MILITARY_UNIT, DATE_OF_BIRTH, DATE_OF_DEATH, STATUS, OBITUARY, CEMETERY_SECTION, RELIGION);
 	}
 
 	@Test
-	public void shouldInstantiateAPerson() {
+	public void shouldInstantiateAGraveSite() {
 		assertNotNull(underTest);
 	}
 
 	@Test
-	public void shouldConstructAndReturnPersonName() {
+	public void shouldConstructAndReturnTheNameOfAPersonInAGraveSite() {
 		String check = underTest.getName();
 		assertEquals(NAME_OF_PERSON, check);
 	}
 
 	@Test
-	public void shouldReturnPersonDateOfBirth() {
+	public void shouldReturnDateOfBirthOfAPersonInAGraveSite() {
 		String check = underTest.getDateOfBirth();
 		assertEquals(DATE_OF_BIRTH, check);
 	}
 
 	@Test
-	public void shouldReturnPersonDateOfDeath() {
+	public void shouldReturnDateOfDeathOfAPersonInAGraveSite() {
 		String check = underTest.getDateOfDeath();
 		assertEquals(DATE_OF_DEATH, check);
 	}
 
 	@Test
-	public void shouldReturnPersonStatus() {
+	public void shouldReturnTheStatusOfAPersonInAGraveSite() {
 		String check = underTest.getStatus();
 		assertEquals(STATUS, check);
 	}
 
 	@Test
-	public void shouldReturnPersonDescription() {
-		String check = underTest.getDrescription();
-		assertEquals(DESCRIPTION, check);
+	public void shouldReturnTheObituaryOfAPersonInAGraveSite() {
+		String check = underTest.getObituary();
+		assertEquals(OBITUARY, check);
 	}
 
 	@Test
-	public void shouldAddReligionToPerson() {
+	public void shouldAddReligionToThePersonInAGraveSite() {
 		String check = underTest.getReligion();
 		assertEquals(RELIGION, check);
 	}
 
 	@Test
-	public void shouldAddTombstoneImageToPerson() {
+	public void shouldAddTombstoneImageOfAPersonInAGraveSite() {
 		String check = underTest.getTombstoneImageUrl();
 		assertEquals(TOMBSTONE_IMAGE_URL, check);
 	}
 
 	@Test
-	public void shouldAddAUsStateToPerson() {
+	public void shouldAddAUsStateToThePersonOfAGraveSite() {
 		String check = underTest.getUsState();
 		assertEquals(US_STATE, check);
 	}
 
 	@Test
-	public void shouldAddMilitaryRankToPerson() {
+	public void shouldAddMilitaryRankToThePersonOfAGraveSite() {
 		String check = underTest.getMilitaryRank();
 		assertEquals(MILITARY_RANK, check);
 	}
 
 	@Test
-	public void shouldAddMilitaryBranchToPerson() {
+	public void shouldAddMilitaryBranchToThePersonOfAGraveSite() {
 		String check = underTest.getMilitaryBranch();
 		assertEquals(MILITARY_BRANCH, check);
 	}
 
 	@Test
-	public void shouldAddMilitaryUnitToPerson() {
+	public void shouldAddMilitaryUnitToThePersonOfAGraveSite() {
 		String check = underTest.getMilitaryUnit();
 		assertEquals(MILITARY_UNIT, check);
 	}
