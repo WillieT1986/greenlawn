88caa45b826060f0c874a1bd4c8e7cea15461124
WillieT1986 <warmad2@hotmail.com>
2019-03-28 22:48:11 -0400

    Refactored and Polished.

--- src/main/java/com/wrthompsonjr/greenlawn/GraveSiteRepository.java
+++ src/main/java/com/wrthompsonjr/greenlawn/GraveSiteRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PersonRepository extends JpaRepository<Person, Long> {
+public interface GraveSiteRepository extends JpaRepository<GraveSite, Long> {
 
 }
