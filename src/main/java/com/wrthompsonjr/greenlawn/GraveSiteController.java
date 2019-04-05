8a9cf7c66ab9063f5793c497c935362e4f6a3efe
WillieT1986 <warmad2@hotmail.com>
2019-04-05 17:56:35 -0400

    Changed getAll() to findAll(). Worked better and successfully ran the program.

--- src/main/java/com/wrthompsonjr/greenlawn/GraveSiteController.java
+++ src/main/java/com/wrthompsonjr/greenlawn/GraveSiteController.java
@@ -4,8 +4,9 @@ import javax.annotation.Resource;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class GraveSiteController {
@@ -19,38 +20,44 @@ public class GraveSiteController {
 	@Resource
 	private TagRepository tagRepo;
 
-	@RequestMapping(value = "graveSites")
+	@ResponseBody
+	@GetMapping(value = "graveSites")
 	public String findAllGraveSites(@RequestParam Long id, Model model) {
-		model.addAttribute("graveSites", graveSiteRepo.getAll());
+		model.addAttribute("graveSites", graveSiteRepo.findAll());
 		return "graveSites";
 
 	}
 
-	@RequestMapping("graveSite")
+	@ResponseBody
+	@GetMapping("graveSite")
 	public String findOneGraveSite(@RequestParam long id, Model model) {
 		model.addAttribute("graveSite", graveSiteRepo.getOne(id));
 		return "graveSite";
 	}
 
-	@RequestMapping(value = "cemeterySections")
+	@ResponseBody
+	@GetMapping(value = "cemeterySections")
 	public String findAllCemeterySections(@RequestParam Long id, Model model) {
-		model.addAttribute("cemeterySections", cemeterySectionRepo.getAll());
+		model.addAttribute("cemeterySections", cemeterySectionRepo.findAll());
 		return "cemeterySections";
 	}
 
-	@RequestMapping("cemeterySection")
+	@ResponseBody
+	@GetMapping("cemeterySection")
 	public String findOneCemeterySection(@RequestParam long id, Model model) {
 		model.addAttribute("cemeterySection", cemeterySectionRepo.getOne(id));
 		return "cemeterySection";
 	}
 
-	@RequestMapping(value = "tags")
+	@ResponseBody
+	@GetMapping(value = "tags")
 	public String getAllTags(Model model) {
-		model.addAttribute("tags", tagRepo.getAll());
+		model.addAttribute("tags", tagRepo.findAll());
 		return "tags";
 	}
 
-	@RequestMapping("tag")
+	@ResponseBody
+	@GetMapping("tag")
 	public String findOneTag(@RequestParam long id, Model model) {
 		model.addAttribute("tag", tagRepo.getOne(id));
 		return "tag";
