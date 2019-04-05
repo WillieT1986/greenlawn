633ea988ed206edb9e5f7d7fb2e943dc96af4d72
WillieT1986 <warmad2@hotmail.com>
2019-04-05 10:39:24 -0400

    Added method for request mapping to find a single grave site.

--- src/main/java/com/wrthompsonjr/greenlawn/GraveSiteController.java
+++ src/main/java/com/wrthompsonjr/greenlawn/GraveSiteController.java
@@ -1,5 +1,22 @@
 package com.wrthompsonjr.greenlawn;
 
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
 public class GraveSiteController {
+
+	@Resource
+	private GraveSiteRepository graveSiteRepo;
+
+	@RequestMapping("graveSite")
+	public String findOneGraveSite(@RequestParam long id, Model model) {
+		model.addAttribute("graveSite", graveSiteRepo.getOne(id));
+		return "graveSite";
+	}
 
 }
