7a5766fd20b1e8a904e2ccde67e9b6c0eab5222b
WillieT1986 <warmad2@hotmail.com>
2019-04-14 01:38:47 -0400

    Changed to RequestMapping.

--- src/main/java/com/wrthompsonjr/greenlawn/controllers/TagController.java
+++ src/main/java/com/wrthompsonjr/greenlawn/controllers/TagController.java
@@ -4,9 +4,8 @@ import javax.annotation.Resource;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.wrthompsonjr.greenlawn.data.TagRepository;
 
@@ -16,15 +15,13 @@ public class TagController {
 	@Resource
 	private TagRepository tagRepo;
 
-	@ResponseBody
-	@GetMapping(value = "tags")
+	@RequestMapping(value = "tags")
 	public String getAllTags(Model model) {
 		model.addAttribute("tags", tagRepo.findAll());
 		return "tags";
 	}
 
-	@ResponseBody
-	@GetMapping("tag")
+	@RequestMapping("tag")
 	public String findOneTag(@RequestParam Long id, Model model) {
 		model.addAttribute("tag", tagRepo.getOne(id));
 		return "tag";
